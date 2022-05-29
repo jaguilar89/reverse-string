ec4b6f3b034812c88b2615b065381ccd375ac25f
Jose Aguilar <jaguilarny@gmail.com>
2022-05-29 14:35:04 -0400

    Passed tests

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,10 @@
 function reverseString(str) {
-  // type your code here
+  let reversedStr = '';
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedStr += str[i];
+  }
+  return reversedStr;
 }
 
 if (require.main === module) {
@@ -17,3 +22,17 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+/*
+I need to write a function that takes one input, a string. The function should take the string input and output that same string
+in reverse. Ie. Input => 'cat' = Output => 'tac'.
+
+Edge cases:
+- String with a single letter: ex. 'a'
+- Empty string?? ex. ' '
+
+declare variable set to empty string
+iterate through string in reverse order
+  for each letter in string, starting with the last letter
+  add letter to empty string variable
+  */
